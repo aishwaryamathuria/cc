5466f78568d9c7b392fb5cf3f07d126fe1612459
Aishwarya Mathuria <mathuria@adobe.com>
2024-03-14 21:55:08 +0530

    Update start-over.js

--- creativecloud/features/interactive-components/start-over/start-over.js
+++ creativecloud/features/interactive-components/start-over/start-over.js
@@ -3,6 +3,11 @@ import { getLibs } from '../../../scripts/utils.js';
 export default async function stepInit(data) {
   const miloLibs = getLibs('/libs');
   const { createTag } = await import(`${miloLibs}/utils/utils.js`);
+  const img = data.config.querySelector('img');
+  if (!img.src.includes('svg')) {
+    const imgclone = img.closest('picture').cloneNode(true);
+    data.target.querySelector('picture').replaceWith(imgclone);
+  }
   const svg = data.config.querySelector('img[src*=svg]');
   const svgClone = svg.cloneNode(true);
   const content = 'Start Over';
